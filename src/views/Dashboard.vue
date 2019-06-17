a4f725ee030148e55591782c2903a3dc567ce68e
Alex Clarke <Alex.Clarke@jdplc.com>
2019-06-17 12:15:06 +0100

    Remove default components and create 'Dashboard' component.

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="className">
+    <h1 :class="`${className}__title`">Picture Test</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+
+  data: () => ({
+    className: 'dashboard',
+  }),
+};
+</script>
